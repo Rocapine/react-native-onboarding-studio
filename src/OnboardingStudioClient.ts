9649163d783d20d7a4c2cbc48f8499c7f216fa7e
Sammy Teillet <sammy.teillet@gmail.com>
2025-10-02 15:46:35 +0200

    feature: fix signature of constructor

--- src/OnboardingStudioClient.ts
+++ src/OnboardingStudioClient.ts
@@ -1,18 +1,28 @@
 import { GetStepsParams, GetStepsResponse } from "./types";
 
+type OnboardingStudioClientOptions = {
+  baseUrl?: string;
+  isSanbdox?: boolean;
+};
+
 export class OnboardingStudioClient {
   private baseUrl: string;
+  private projectId: string;
+  private options: OnboardingStudioClientOptions;
 
-  constructor(baseUrl?: string) {
-    console.info("OnboardingStudioClient constructor", baseUrl);
+  constructor(projectId: string, options: OnboardingStudioClientOptions) {
+    console.info("OnboardingStudioClient constructor", options.baseUrl);
+    this.projectId = projectId;
+    this.options = options;
     this.baseUrl =
-      baseUrl || "https://takbcvjljqialzqyksic.supabase.co/functions/v1";
+      options.baseUrl ||
+      "https://takbcvjljqialzqyksic.supabase.co/functions/v1";
   }
 
   async getSteps(params: GetStepsParams): Promise<GetStepsResponse> {
     console.info("OnboardingStudioClient getSteps", params);
-    const { projectId } = params;
-    const url = `${this.baseUrl}/get-onboarding-steps?projectId=${projectId}`;
+    const isSandbox = this.options.isSanbdox;
+    const url = `${this.baseUrl}/get-onboarding-steps?projectId=${this.projectId}?draft=${isSandbox}`;
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(
