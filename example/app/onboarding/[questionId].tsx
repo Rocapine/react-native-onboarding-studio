d03c4cfcc2e6e3e7627a49c2b63561fa59225705
Sammy Teillet <sammy.teillet@gmail.com>
2025-10-06 17:36:40 +0200

    feature: add simple nav for onboarding

---
+++ example/app/onboarding/[questionId].tsx
@@ -0,0 +1,36 @@
+import * as OnboardingStudio from '@rocapine/react-native-onboarding-studio';
+import { useLocalSearchParams } from 'expo-router';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function QuestionPage() {
+  const { questionId } = useLocalSearchParams();
+  console.log('questionId', questionId);
+  const stepPayload = {
+    title: 'Why users love us',
+    subtitle: 'Simple, effective, and reliable',
+    socialProofs: [
+      { numberOfStar: 5, content: 'Finally found a nutrition app that’s helping me hit my fitness goals. It’s all about eating the right foods and staying disciplined. I’ve already seen real changes in my body in just a month!', authorName: 'Emma Davis' },
+      { numberOfStar: 4, content: 'Intuitive and powerful.', authorName: 'Mike Chen' },
+      { numberOfStar: 3, content: 'This app has completely transformed how I manage my daily tasks. Highly recommended!', authorName: 'Sarah Johnson' },
+      { numberOfStar: 2, content: 'Great app!', authorName: 'Lisa Anderson' },
+      { numberOfStar: 1, content: 'Love it!', authorName: 'David Martinez' },
+    ],
+  } satisfies OnboardingStudio.RatingsStep['payload'];
+  const step = {
+    id: '1',
+    type: 'Ratings',
+    name: 'Ratings',
+    displayProgressHeader: true,
+    payload: stepPayload,
+    customPayload: null,
+    figmaUrl: null,
+  } satisfies OnboardingStudio.RatingsStep;
+
+  // return <Text>Hello</Text>
+  return (
+    <OnboardingStudio.RatingsRenderer step={step} />
+  );
+}
