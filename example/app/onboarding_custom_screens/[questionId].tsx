4a480e6cddb98e48c475b1f3cd831c6c306fcc4f
Sammy Teillet <sammy.teillet@gmail.com>
2025-10-23 15:53:00 +0200

    example: add onboarding with custom steps

---
+++ example/app/onboarding_custom_screens/[questionId].tsx
@@ -0,0 +1,37 @@
+import {
+  useOnboardingStep,
+  OnboardingPage,
+  LoaderStepType,
+  CommitmentStepType,
+} from "@rocapine/react-native-onboarding-studio";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { OnboardingStep } from "./types";
+
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
+
+
+
+export default function QuestionPage() {
+  const { questionId } = useLocalSearchParams();
+  console.log("questionId", questionId);
+  const { step, isLastStep, onboardingMetadata } = useOnboardingStep<OnboardingStep>({
+    stepNumber: parseInt(questionId as string, 10),
+  });
+
+
+  const router = useRouter();
+
+  const onContinue = (args?: any) => {
+    if (isLastStep) {
+      router.push("/");
+      return;
+    } else {
+      router.push(`/onboarding/${parseInt(questionId as string, 10) + 1}`);
+    }
+  };
+
+  return <OnboardingPage step={step} onContinue={onContinue} />;
+}
